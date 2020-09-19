984427dbe8d7df61d0a23d6be955d343806f8391
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-09-19 20:30:11 +0900

    Create Drawing_Turtle2.py

---
+++ CH1/Drawing_Turtle2.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.circle(100)   #반지름이 100인 원
+
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
