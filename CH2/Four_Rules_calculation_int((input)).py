bf777147a92020f37405576b34049904adc167ae
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-09-20 12:38:00 +0900

    Create Four_Rules_calculation_int((input)).py

---
+++ CH2/Four_Rules_calculation_int((input)).py
@@ -0,0 +1,7 @@
+x = int(input("첫번째 정수 x를 입력하세요: "))
+y = int(input("두번째 정수 y를 입력하세요: "))
+
+print("x + y = ", x + y, "입니다.")
+print("x - y = ", x - y, "입니다")
+print("x X y = ", x * y, "입니다")
+print("x / y = ", x / y, "입니다")
