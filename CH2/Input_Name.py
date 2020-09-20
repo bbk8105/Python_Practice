58c9a84c7705cedadebcfcd4f77bad5dea000cc7
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-09-20 12:45:59 +0900

    Create Input_Name.py

---
+++ CH2/Input_Name.py
@@ -0,0 +1,10 @@
+name = input("이름을 입력하세요: ")
+
+print(name, "씨 안녕하세요?")
+
+x = int(input("첫번째 정수를 입력하세요: "))
+y = int(input("두번째 정수를 입력하세요: "))
+
+sum = x + y
+
+print(x, "와", y, "의 합은", sum, "입니다.")
