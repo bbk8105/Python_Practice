ea0dacdbf35550861f84ea112e78f420ff50be16
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-03 23:40:26 +0900

    Create Real_Estate_Advertising(variable practice).py

---
+++ CH2/Real_Estate_Advertising(variable practice).py
@@ -0,0 +1,15 @@
+street = "충남 공주시"
+type = "주택"
+number_of_room = 4
+price = 100000000
+
+print("#################")
+print("#               #")
+print(" 부동산 매물광고 ")
+print("#               #")
+print("#################")
+
+print("")
+
+print(street, "에 위치한 아주 좋은", type, "이 매물로 나왔습니다.")
+print("이", type, "은", number_of_room, "개의 방을 가지고 있으며 가격은", price, "원 입니다.")
