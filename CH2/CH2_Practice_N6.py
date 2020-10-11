61f470f1a9420dc08554b0dc2c97863cc89192d8
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-11 15:29:04 +0900

    Create CH2_Practice_N6.py

---
+++ CH2/CH2_Practice_N6.py
@@ -0,0 +1,24 @@
+# 우리는 CH_2 5번 문제 코드에서 삼각형 한 변의 길이를 side 변수로 표시했었다.
+# 만약 우리가 삼각형 안의 한 변의 길이를 200으로 변경한다고 해보자.
+# CH_2 5번 문제 코드에서 어디만 수정해주면 되는가?
+
+
+# CH_2 5번 문제 코드
+
+"""
+
+import turtle
+
+t = turtle.Turtle()
+
+side = 100
+
+t.forward(side)
+t.left(120)
+t.forward(side)
+t.left(120)
+t.forward(120)
+
+"""
+
+# 해답: side 변수를 100이 아닌 200으로 설정한다.
