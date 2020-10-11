2195ab90e90c741553592124002eb57b64cb6c60
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-11 15:20:12 +0900

    Create CH2_Practice_N5.py

---
+++ CH2/CH2_Practice_N5.py
@@ -0,0 +1,15 @@
+# 삼각형의 한 변의 길이를 side 변수로 나타낸다.
+# side 변수의 초기값은 100이다.
+# side 변수를 이용하여 화면에 삼각형을 그려보자.
+
+import turtle
+
+t = turtle.Turtle()
+
+side = 100
+
+t.forward(side)
+t.left(120)
+t.forward(side)
+t.left(120)
+t.forward(side)
