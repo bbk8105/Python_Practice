0e2128bf6e3555175413c0f0779a9f0e7ef09534
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-26 02:51:27 +0900

    Create CH3_Practice_N1.py

---
+++ CH3/CH3_Practice_N1.py
@@ -0,0 +1,9 @@
+x = int(input("첫 번째 정수를 입력하시오: "))
+y = int(input("두 번째 정수를 입력하시오: "))
+
+print("두 정수의 합 =", x + y)
+print("두 정수의 차 =", x - y)
+print("두 정수의 곱 =", x * y)
+print("두 정수의 평균=", (x + y) / 2)
+print("두 정수 중 큰 수 =", max(x, y))
+print("두 정수 중 작은 수 =", min(x, y))
