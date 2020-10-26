ec127e975ada23977ceb7707a282026093f55054
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-27 01:25:17 +0900

    Create Turtle_Color_List.py

---
+++ CH4/Turtle_Color_List.py
@@ -0,0 +1,28 @@
+import turtle
+
+t = turtle.Turtle()
+
+color_list = [ "yellow", "red", "blue", "green" ]
+
+t.fillcolor(color_list[0])
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+t.fd(50)
+
+t.fillcolor(color_list[1])
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+t.fd(50)
+
+t.fillcolor(color_list[2])
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+t.fd(50)
+
+t.fillcolor(color_list[3])
+t.begin_fill()
+t.circle(100)
+t.end_fill()
