3e94891bf7cbe93b584b741394e80a3616d91673
Jun K <69249345+bbk8105@users.noreply.github.com>
2020-10-27 01:15:33 +0900

    Create Friends_List.py

---
+++ CH4/Friends_List.py
@@ -0,0 +1,20 @@
+F_list = []
+
+F = input("친구의 이름을 입력하시오: ")
+F_list.append(F)
+
+F = input("친구의 이름을 입력하시오: ")
+F_list.append(F)
+
+F = input("친구의 이름을 입력하시오: ")
+F_list.append(F)
+
+F = input("친구의 이름을 입력하시오: ")
+F_list.append(F)
+
+F = input("친구의 이름을 입력하시오: ")
+F_list.append(F)
+
+print(F_list)
+
+
